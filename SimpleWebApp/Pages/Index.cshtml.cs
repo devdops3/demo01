157fde81966e294b86d191cf57c0da1275465bdb
devdops3 <devdops3@gmail.com>
2024-05-21 16:00:27 +0630

    fix code

--- SimpleWebApp/Pages/Index.cshtml.cs
+++ SimpleWebApp/Pages/Index.cshtml.cs
@@ -1,12 +1,16 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace SimpleWebApp.Pages
 {
-    public class PrivacyModel : PageModel  // Added opening brace
+    public class IndexModel : PageModel
     {
+        public string Message { get; private set; } = "PageModel in C#";
+
+        // This method runs when the page is accessed with a GET request
         public void OnGet()
         {
+            Message += $" Server time is {DateTime.Now}";
         }
     }
 }
